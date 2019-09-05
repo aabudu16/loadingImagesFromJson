9694fc95916b350b8cf2be01f1c7ea6e355c209b
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.local>
2019-09-05 16:05:43 -0400

    Adds tab bar controller with three view controllers

---
+++ LoadingImageLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadingImageLab
+//
+//  Created by Mr Wonderful on 9/5/19.
+//  Copyright © 2019 Mr Wonderful. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
